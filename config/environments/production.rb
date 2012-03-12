7c09fdace9ffe468877fb5bd259f40667af49b67
Your Name <paulanthonywilson@gmail.com>
2012-03-12 00:37:21 +0000

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ SubmissionReview::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(jquery.tooltip.css print.css reset-fonts-grids.css)
+  config.assets.precompile += %w(jquery.tooltip.css print.css reset-fonts-grids.css tablesort.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
